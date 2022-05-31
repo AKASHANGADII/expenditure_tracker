ddedded1b5543c410e2232c70bf5e6a08306f559
akashangadii <akashakku2202@gmail.com>
2022-05-31 23:52:15 +0530

    Removed the week tracker container

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,8 +3,6 @@ import 'package:expenditure_tracker/widgets/user_expenditure.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +14,6 @@ class HomeScreen extends StatelessWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Expanded(
-              child: Container(
-                color: Colors.orange,
-                child: Text("Place Holder"),
-              ),
-            ),
             ExpenditureData(),
           ],
         ),
